3fa2f3b9d8403057cb78609d3fe474867ee47f1b
animallibraryn4 <nikhil7858978052@gmail.com>
2025-03-19 04:42:57 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -1,4 +1,5 @@
 import random
+from pyrogram.types import ForceReply
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, Message
 
